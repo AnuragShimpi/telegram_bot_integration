0773f9dfc5a8bac34f2a585de7465b9f04ca2e50
AnuragShimpi <anuragshimpi91@gmail.com>
2024-06-25 12:34:32 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+from langchain_cohere import ChatCohere
+from langchain.agents import AgentType, initialize_agent, load_tools
+ 
+app = FastAPI()
+ 
+class QueryRequest(BaseModel):
+    query: str
+ 
+# Set environment variables for API keys
+os.environ['cohere_api_key'] = "PKYJ9mWpadwNW8Oj44ftpOr6rKzBkzW6eT2iZhaC"
+os.environ['OPENWEATHERMAP_API_KEY'] = "ccb2a2627f1646603dbc9721e03990dc"
+os.environ['serpapi_api_key'] = "73ce724cbce1469407bd4191a7b7c54aba5366019305d9f0f05f61791404f023"
+ 
+ 
+def search_query(query: str):
+    custom_prompt = (
+        "You are a knowledgeable assistant. When asked about any topic, provide a detailed and comprehensive response. "
+        "Include background information, key points, significant achievements, relevant current events, and any controversies. "
+        "Ensure your answer is well-rounded and informative."
+    )
+ 
+    llm = ChatCohere(model='command-r-plus', temperature=0)
+    tools = load_tools(["serpapi" , 'openweathermap-api'], llm)
+ 
+    agent_chain = initialize_agent(
+        tools=tools,
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        prompt=custom_prompt
+    )
+    result = agent_chain.run(query)
+    return result
+ 
+ 
+@app.post("/search")
+async def search(query: QueryRequest):
+    try:
+        result = search_query(query.query)
+        return {"result": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+ 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ telegram_bot.py
@@ -0,0 +1,76 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import os
+import requests
+from config import *
+ 
+TOKEN: Final = telegram_api
+# BOT_USERNAME: Final = '@GenElvebot'
+ 
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! I am your go-to chatbot for instant weather updates and information searches, providing you with accurate and timely results.")
+ 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("""Hello! I am your go-to chatbot for instant weather updates and information searches. Here's how you can use me:
+                                    \n\tWeather Updates: Just type "weather" followed by the city name (e.g., "weather in New York") to get the latest weather information.
+                                    \n\tSearch Information: Ask me any question or search for any topic (e.g., "latest news on technology"), and I'll provide you with accurate and up-to-date results.
+                                    \nIf you need further assistance, feel free to ask!""")
+ 
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is a custom command!')
+ 
+# Handle response
+ 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+ 
+    # print("message type :: ",message_type)
+    
+    print(f"user id :: ({update.message.chat.id}) user name :: ({update.message.chat.username})")
+    print("text message :: ",text)
+ 
+    result = call_search_api2(text)
+    print('Bot:', result)
+    await update.message.reply_text(result)
+ 
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+ 
+ 
+base_url = "http://0.0.0.0:8000" # - query - result
+ 
+# base_url = "https://webapp-be-nlq.azurewebsites.net"  # - input - output
+ 
+def call_search_api2(query:str):
+    payload = {"query": query}
+    print("hiii")
+    response = requests.post(f"{base_url}/search_query", json=payload)
+    if response.status_code == 200:
+        return response.json().get("result")
+    else:
+        return f"Failed to call search API: {response.status_code}, {response.text}"
+ 
+ 
+# ------------------------------------------
+ 
+ 
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+ 
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+ 
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+ 
+    # Errors
+    app.add_error_handler(error)
+ 
+    # increased timeout for update
+    print("Executing....")
+    app.run_polling(poll_interval=3, timeout=10)  # Increased timeout to 10 seconds
